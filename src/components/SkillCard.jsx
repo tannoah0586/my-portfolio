0790530958c7fad92f382a82a4d16d18efec9950
tannoah <tannoah@me.com>
2025-06-04 19:38:51 +0800

    added stuff i cannot remember

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,13 +1,15 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SkillCard =({ skill })=>{
-    return (
-        <li style={{ display: "flex", alignItems: "center", gap: "0.5rem", marginBottom: "0.5rem" }}>
-            <FontAwesomeIcon icon={skill.icon} />
-            {skill.name}
-        </li>
-    );
+const SkillCard = ({ skill }) => {
+  return (
+    <li style={{ display: "flex", alignItems: "center", gap: "0.5rem", marginBottom: "0.5rem" }}>
+      <FontAwesomeIcon
+        icon={skill.icon}
+        style={{ color: skill.color, fontSize: "1.5rem" }}
+      />
+      {skill.name}
+    </li>
+  );
 };
 
 export default SkillCard;
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,24 +1,38 @@
-import { faHtml5, faCss3Alt, faJs, faReact, faNode, faPython, faGitAlt, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faDatabase, faCode, faServer } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faReact,
+  faNode,
+  faPython,
+  faGitAlt,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faDatabase,
+  faCode,
+  faServer
+} from "@fortawesome/free-solid-svg-icons";
 
 const skills = {
   frontend: [
-    { name: "HTML", icon: faHtml5 },
-    { name: "CSS", icon: faCss3Alt },
-    { name: "JavaScript", icon: faJs },
-    { name: "React", icon: faReact }
+    { name: "HTML", icon: faHtml5, color: "#e34c26" },
+    { name: "CSS", icon: faCss3Alt, color: "#264de4" },
+    { name: "JavaScript", icon: faJs, color: "#f7df1e" },
+    { name: "React", icon: faReact, color: "#61dbfb" }
   ],
   backend: [
-    { name: "Node.js", icon: faNode },
-    { name: "Python", icon: faPython },
-    { name: "MongoDB", icon: faDatabase }
+    { name: "Node.js", icon: faNode, color: "#3c873a" },
+    { name: "Python", icon: faPython, color: "#3776ab" },
+    { name: "MongoDB", icon: faDatabase, color: "#47A248" } // Not official but close
   ],
   tools: [
-    { name: "Git", icon: faGitAlt },
-    { name: "GitHub", icon: faGithub },
-    { name: "VS Code", icon: faCode },
-    { name: "Netlify", icon: faServer }
+    { name: "Git", icon: faGitAlt, color: "#f34f29" },
+    { name: "GitHub", icon: faGithub, color: "#181717" },
+    { name: "VS Code", icon: faCode, color: "#007ACC" },
+    { name: "Netlify", icon: faServer, color: "#00C7B7" } // Custom color
   ]
 };
 
 export default skills;
+
