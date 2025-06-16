494704dbc944b3491429290ee52af5c469c2c786
tannoah <tannoah@me.com>
2025-06-16 19:35:21 +0800

    reorder projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,14 +6,15 @@
       github: "https://github.com/tannoah0586/crypto-tracker",
       live: "https://airtable.com/appyRM0vuSW9VXrza/pagO1vYLtnyrsbcHZ/form"
     },
-    {
-      title: "Singlish Wordle Game",
-      description: "A Wordle-inspired browser game where players guess 5-letter Singlish words. Features a custom word list, visual feedback for correct and misplaced letters, and a clean, responsive design.",
-      tech: ["JavaScript", "HTML", "CSS"],
-      github: "https://github.com/tannoah0586/Wordle_Game",
-      live: "https://dynamic-axolotl-7344c7.netlify.app"
+      {
+      title: "AWB Tracker",
+      description: "Developed a React, Express, MongoDB (MERN) application to streamline Air Waybill (AWB) management. Enables users to track cargo, update statuses, and manage sender/receiver information for logistics.",
+      tech: ["React", "Node.js", "Express", "MongoDB", "JavaScript", "HTML", "CSS"],
+      githubFrontend: "https://github.com/tannoah0586/AWB-tracker2-FE",
+      githubBackend: "https://github.com/tannoah0586/AWB-Tracker2-BE",
+      live: "https://awb-tracker2.vercel.app/"
     },
-    {
+      {
       title: "Company Ideas Incubator Tool",
       description: "Full-stack MERN idea-sharing platform where users can submit, browse, and rate project concepts. Includes secure authentication, core CRUD operations, and interactive feedback.",
       tech: ["MongoDB", "Express", "React", "Node.js", "JavaScript", "HTML", "CSS"],
@@ -22,13 +23,14 @@
       live: "https://ideas-incubator.vercel.app/",
     },
     {
-      title: "AWB Tracker",
-      description: "Developed a React, Express, MongoDB (MERN) application to streamline Air Waybill (AWB) management. Enables users to track cargo, update statuses, and manage sender/receiver information for logistics.",
-      tech: ["React", "Node.js", "Express", "MongoDB", "JavaScript", "HTML", "CSS"],
-      githubFrontend: "https://github.com/tannoah0586/AWB-tracker2-FE",
-      githubBackend: "https://github.com/tannoah0586/AWB-Tracker2-BE",
-      live: "https://awb-tracker2.vercel.app/"
+      title: "Singlish Wordle Game",
+      description: "A Wordle-inspired browser game where players guess 5-letter Singlish words. Features a custom word list, visual feedback for correct and misplaced letters, and a clean, responsive design.",
+      tech: ["JavaScript", "HTML", "CSS"],
+      github: "https://github.com/tannoah0586/Wordle_Game",
+      live: "https://dynamic-axolotl-7344c7.netlify.app"
     },
+
+
   ];
 
 export default projects;
